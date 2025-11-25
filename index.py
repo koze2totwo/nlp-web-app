5ba5cf789ae4353778b6f722e00fd49de311e9d3
Tshantu <shantudhami@gmail.com>
2025-11-25 19:08:32 +0530

    progress

--- index.py
+++ index.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from db import Database
 app=Flask(__name__)
+dbo=Database()
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/perform_registration',methods=['post'])
+def perform_registration():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    response = dbo.insert(name,email,password)
+    if response:
+        return render_template('login.html',message="Registration successful")
+    else:
+        return render_template('register.html',message="Email already exist")
+@app.route('/perform_login',methods=['post'])
+def perform_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    response = dbo.search(email,password)
+    if(response):
+        return "successful"
+    else:
+        return render_template('login.html',m='incorrect email/password')
+
 app.run(debug=True)
